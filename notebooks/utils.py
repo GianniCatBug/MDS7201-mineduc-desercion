d2a964027e22e2698528053cb254080bed7ad93e
GianniCatBug <gianinasl@gmail.com>
2022-09-07 14:29:11 -0400

    vevtor objetivo caso 2

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import numpy as np
 
 def iter_df_read(file, col_name, condition_list, chunksize=1000, **kwargs):
     iter_csv = pd.read_csv(file, iterator=True, chunksize=chunksize, **kwargs)
     return pd.concat([chunk[chunk[col_name].isin(condition_list)] for chunk in iter_csv])
+
+def get_array_diff(x):
+    tmp = np.diff(x) 
+    return tmp.max() if len(tmp) else 0
